782d549e5e97bc39abe43adc6ae7b892e3f5ed3a
Francisco Javier <fjmr17@hotmail.com>
2015-11-16 12:47:07 -0600

    Copia de Floreria Ventas
    Aqui me podrias ayudar porfavor

---
+++ menu.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title></title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+
+<div id="menu">
+<div id="sidemenu">
+<h2>Menu</h2>
+<ul>
+<li class="active"><a href="#">INICIO</a> </li>
+<li><a href="armaProducto.php">ARMA TU PRODUCTO</a></li>
+<li><a href="catalogo.php">CATALOGO</a></li>
+<li><a href="carrito.php">CARRITO</a></li>
+<li><a href="nosotros.php">NOSOTROS</a></li>
+<li><a href="contacto.php">CONTACTO</a></li>
+<li><a href="seccionPago.php">SECCION DE PAGO (SOLO ES MIENTRAS ESTA LA SECCION DE CARRITO)</a></li>
+</ul>
+</div>
+</div>
+</body>
+</html>
