6dea636bb111db2fe5e3630108c1e177e57bb1b9
alansa <sandi_ico@hotmail.com>
2015-11-16 19:32:17 -0600

    comienzo de estilos
    :D

--- inicio/tabla.php
+++ inicio/tabla.php
@@ -1,33 +1,46 @@
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="menu/tabla.css">
+	<link href="inicio/tabla.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<br></br>
-	<table>
+	<!-- Comentarios -->
+	<table id="tabla">  
 		<tr>
-			<td>Primer arreglo</td>
-			<td>Segundo arreglo</td>
-			<td>Tercer arreglo</td>
-			<td>Cuarto arreglo</td>
+			<th colspan="4">PRIMAVERA</th>
+		</tr>  
+		<tr>  <!-- La etiqueta tr son las fila -->
+			<td id="celda">Primer arreglo</td>  <!-- La etiqueta td son las columnas-->
+			<td id="celda">Segundo arreglo</td>
+			<td id="celda">Tercer arreglo</td>
+			<td id="celda">Cuarto arreglo</td>
 		</tr>
 		<tr>
-			<td>Quinto arreglo</td>
-			<td>Sexto arreglo</td>
-			<td>Septimo arreglo</td>
-			<td>Octavo arreglo</td>
+			<th colspan="4">10 DE MAYO</th>
 		</tr>
 		<tr>
-			<td>Noveno arreglo</td>
-			<td>Decimo arreglo</td>
-			<td>11 arreglo</td>
-			<td>12 arreglo</td>
+			<td id="celda">Quinto arreglo</td>
+			<td id="celda">Sexto arreglo</td>
+			<td id="celda">Septimo arreglo</td>
+			<td id="celda">Octavo arreglo</td>
 		</tr>
 		<tr>
-			<td>13 arreglo</td>
-			<td>14 arreglo</td>
-			<td>15 arreglo</td>
-			<td>16 arreglo</td>
+			<th colspan="4">SAN VALENTIN</th>
+		</tr>
+		<tr>
+			<td id="celda">Noveno arreglo</td>
+			<td id="celda">Decimo arreglo</td>
+			<td id="celda">11 arreglo</td>
+			<td id="celda">12 arreglo</td>
+		</tr>
+		<tr>
+			<th colspan="4">CUMPLEAÑOS</th>
+		</tr>
+		<tr>
+			<td id="celda">13 arreglo</td>
+			<td id="celda">14 arreglo</td>
+			<td id="celda">15 arreglo</td>
+			<td id="celda">16 arreglo</td>
 		</tr>
 
 	</table>
