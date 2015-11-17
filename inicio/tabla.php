452ae4bb74f2f5ed69690c1f4584a5e08ad0a840
alansa <sandi_ico@hotmail.com>
2015-11-17 00:47:45 -0600

    botones

--- inicio/tabla.php
+++ inicio/tabla.php
@@ -10,53 +10,53 @@
 			<th colspan="4">PRIMAVERA</th>
 		</tr>  
 		<tr>  <!-- La etiqueta tr son las fila -->
-			<td id="celda"><img id="imagen" src="inicio/Arreglos/12.jpg">
-				<br>Primer arreglo</td>  <!-- La etiqueta td son las columnas-->
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/12.jpg"> <!-- La etiqueta td son las columnas-->
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/15.jpg">
-				<br>Segundo arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/13.jpg">
-				<br>Tercer arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/10.jpg">
-				<br>Cuarto arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 		</tr>
 		<tr>
 			<th colspan="4">10 DE MAYO</th>
 		</tr>
 		<tr>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/2.jpg">
-				<br>Quinto arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/14.jpg">
-				<br>Sexto arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/1.jpg">
-				<br>Septimo arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/16.jpg">
-				<br>Octavo arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 		</tr>
 		<tr>
 			<th colspan="4">SAN VALENTIN</th>
 		</tr>
 		<tr>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/3.jpg">
-				<br>Noveno arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/4.jpg">
-				<br>Decimo arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/7.jpg">
-				<br>11 arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/9.jpg">
-				<br>12 arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 		</tr>
 		<tr>
 			<th colspan="4">CUMPLEAÑOS</th>
 		</tr>
 		<tr>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/8.jpg">
-				<br>13 arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/5.jpg">
-				<br>14 arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/6.jpg">
-				<br>15 arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 			<td id="celda"><img id="imagen" src="inicio/Arreglos/11.jpg">
-				<br>16 arreglo</td>
+				<br><button type="submit" name="carrito">Agregar a Carrito</button></td>
 		</tr>
 	</table>
 	<br>
