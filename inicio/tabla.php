8a2b51027cc8937037ab778718e6c5a052323ca6
alansa <sandi_ico@hotmail.com>
2015-11-16 21:34:32 -0600

    imagenes integradas

--- inicio/tabla.php
+++ inicio/tabla.php
@@ -10,40 +10,58 @@
 			<th colspan="4">PRIMAVERA</th>
 		</tr>  
 		<tr>  <!-- La etiqueta tr son las fila -->
-			<td id="celda">Primer arreglo</td>  <!-- La etiqueta td son las columnas-->
-			<td id="celda">Segundo arreglo</td>
-			<td id="celda">Tercer arreglo</td>
-			<td id="celda">Cuarto arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/12.jpg">
+				<br>Primer arreglo</td>  <!-- La etiqueta td son las columnas-->
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/15.jpg">
+				<br>Segundo arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/13.jpg">
+				<br>Tercer arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/10.jpg">
+				<br>Cuarto arreglo</td>
 		</tr>
 		<tr>
 			<th colspan="4">10 DE MAYO</th>
 		</tr>
 		<tr>
-			<td id="celda">Quinto arreglo</td>
-			<td id="celda">Sexto arreglo</td>
-			<td id="celda">Septimo arreglo</td>
-			<td id="celda">Octavo arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/2.jpg">
+				<br>Quinto arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/14.jpg">
+				<br>Sexto arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/1.jpg">
+				<br>Septimo arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/16.jpg">
+				<br>Octavo arreglo</td>
 		</tr>
 		<tr>
 			<th colspan="4">SAN VALENTIN</th>
 		</tr>
 		<tr>
-			<td id="celda">Noveno arreglo</td>
-			<td id="celda">Decimo arreglo</td>
-			<td id="celda">11 arreglo</td>
-			<td id="celda">12 arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/3.jpg">
+				<br>Noveno arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/4.jpg">
+				<br>Decimo arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/7.jpg">
+				<br>11 arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/9.jpg">
+				<br>12 arreglo</td>
 		</tr>
 		<tr>
 			<th colspan="4">CUMPLEAÑOS</th>
 		</tr>
 		<tr>
-			<td id="celda">13 arreglo</td>
-			<td id="celda">14 arreglo</td>
-			<td id="celda">15 arreglo</td>
-			<td id="celda">16 arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/8.jpg">
+				<br>13 arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/5.jpg">
+				<br>14 arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/6.jpg">
+				<br>15 arreglo</td>
+			<td id="celda"><img id="imagen" src="inicio/Arreglos/11.jpg">
+				<br>16 arreglo</td>
 		</tr>
-
 	</table>
+	<br>
+	<br>
 
+	<div id="armaArre"> ARMA TU ARREGLO </div>
 </body>
 </html>
